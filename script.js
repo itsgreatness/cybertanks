b065108abce74c4f1367bcd12fde7a4b4f2ad584
itsgreatness <dzhao650@gmail.com>
2023-10-20 18:50:24 -0500

    why is while loop always blocking even in async?

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function () {
     [SHOOT_DELAY, BULLET_SPEED, GRAVITY, TANK_FORWARD_SPEED, BULLET_LIFE, tanks[0].health] = [-1, 160, 15, 80, 999, 1 / 0];
-    const [DT_DELAY, TARGETS,] = [300, 1];
+    const [DT_DELAY, TARGETS] = [300, 1];
     const { sin, cos, PI, sqrt, atan2 } = Math;
     function dist(x1, y1, x2, y2) {
         return sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2);
@@ -8,15 +8,13 @@
     function directionToPoint(x1, y1, x2, y2) {
         return atan2(y2 - y1, x2 - x1) + (y2 < y1 ? PI : 0);
     }
-    let TERMINATE = false;
-    async function pointToATank() {
-        TERMINATE = false;
+    function pointToATank() {
         const targets = tanks.filter(function (t, idx) {
-            return t.pos && t.health < 1 / 0 && !t.isPlane && t.vel[1] > -1 && t.vel[0] + t.vel[2] < 1 && idx > 0
+            return t.pos && t.health < 1 / 0 && !t.isPlane && t.pos[1] === 0 && (t.vel[0] + t.vel[2]) < 1 && idx > 0
         }).sort(function (t1, t2) {
             return dist(tanks[0].pos[0], t1.pos[0], tanks[0].pos[2] - t1.pos[2]) - dist(tanks[0].pos[0], t2.pos[0], tanks[0].pos[2] - t2.pos[2])
         });
-        for (let i = 0; i < TARGETS && !TERMINATE; i++) {
+        for (let i = 0; i < TARGETS; i++) {
             const target = targets.at(i);
             if (!target) continue;
             tanks[0].pos = [target.pos[0] + .08 * BULLET_SPEED * sin(target.rot + PI), target.pos[1], target.pos[2] + .08 * BULLET_SPEED * cos(target.rot + PI)];
@@ -24,7 +22,7 @@
             entities.push(createBullet(tanks[0], -1));
             entities.push(createBullet(tanks[0], 1));
             const t = Date.now();
-            while (Date.now() - startTime < 1000 && !TERMINATE) {
+            while (Date.now() - t < 1000 && target.pos) {
                 tanks[0].rot = directionToPoint(tanks[0].pos[0], tanks[0].pos[2], target.pos[0], target.pos[2]);
             }
         }
@@ -33,8 +31,9 @@
         tanks[0].rotv = .1;
     }
     let t = 0;
-    addEventListener("keydown", async function (e) {
-        e.preventDefault()
+    let shadowFurry = 0;
+    addEventListener("keydown", function (e) {
+        if (e.key.includes("Arrow")) this.clearInterval(shadowFurry);
         console.error("hacks are working");
         if (e.ctrlKey && !e.repeat) {
             console.error("switch mode");
@@ -48,8 +47,8 @@
         if (e.shiftKey && !e.repeat) {
             if (Date.now() - t < DT_DELAY) {
                 console.error("shadow furry");
-                TERMINATE = true;
-                await pointToATank();
+                this.clearInterval(shadowFurry);
+                shadowFurry = this.setTimeout(pointToATank);
             }
             t = Date.now();
             console.error("velocity hacks");
